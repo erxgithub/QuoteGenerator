ece5aa2c7ef2f115e0fd0cd283496db1aac93400
erxgithub <erxlink@gmail.com>
2018-02-07 15:03:12 -0500

    First commit.

---
+++ QuoteGenerator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuoteGenerator
+//
+//  Created by Eric Gregor on 2018-02-07.
+//  Copyright © 2018 Eric Gregor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
